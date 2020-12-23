331336fd7d39109df93ff7859e16f290cabc7464
Jeana Rose <jr.mathis608@gmail.com>
2020-12-23 14:42:53 -0500

    Commented out JS file

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-$(document).ready(function(){
-    var pexelsAPIkey = "563492ad6f91700001000001b70fb799bff84ca4b8a69bfeb2fb60e3";
-})
+// $(document).ready(function(){
+//     var pexelsAPIkey = "563492ad6f91700001000001b70fb799bff84ca4b8a69bfeb2fb60e3";
+// })
