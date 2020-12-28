36cb55a3f89d7373efd12f604daf84a77bc6b829
Jeana Rose <jr.mathis608@gmail.com>
2020-12-28 11:44:11 -0500

    Added select-images.js file, connected it to the select-images.html file, and tested connection

---
+++ select-images.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    var pexelsAPIkey = "563492ad6f91700001000001b70fb799bff84ca4b8a69bfeb2fb60e3";
+
+    var h1Element = $(".display-4");
+    h1Element.attr("style", "color:red");
+})
