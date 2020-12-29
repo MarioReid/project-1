116549458866018c16c65a0ba37d62ce05cec2b1
Jeana Rose <jr.mathis608@gmail.com>
2020-12-29 13:16:28 -0500

    Removed second example question from html. Added console log to target the src url for the first image in the object array

--- assets/js/select-images.js
+++ assets/js/select-images.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
   var APIkey = "563492ad6f91700001000001b70fb799bff84ca4b8a69bfeb2fb60e3";
-  var query = "dogs"
+  var query = "horses"
 
   $.ajax({
     url: "https://api.pexels.com/v1/search?query=" + query,
@@ -9,5 +9,10 @@ $(document).ready(function () {
     headers: { Authorization: APIkey },
   }).then(function (response) {
     console.log(response);
+    console.log(response.photos[0].src.original)
+    // Select image
+    // Create element
+    // Add content
+    // Append to existing element
   });
 });
