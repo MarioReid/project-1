0b6bd4bf5a52048b675c467f9b466e76719b0eb2
Jeana Rose <jr.mathis608@gmail.com>
2020-12-29 12:47:02 -0500

    Added a query variable

--- assets/js/select-images.js
+++ assets/js/select-images.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
 
   var APIkey = "563492ad6f91700001000001b70fb799bff84ca4b8a69bfeb2fb60e3";
+  var query = "dogs";
 
   $.ajax({
-    url: "https://api.pexels.com/v1/search?query=dogs",
+    url: "https://api.pexels.com/v1/search?query=" + query,
     method: "GET",
     headers: { Authorization: APIkey },
   }).then(function (response) {
     console.log(response);
   });
-
 });
