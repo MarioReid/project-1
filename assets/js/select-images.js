01b8a1212dded75181a4cc9aed439a4e86271f94
Jeana Rose <jr.mathis608@gmail.com>
2020-12-29 12:20:13 -0500

    Modified select-images.js to make API work correctly

--- assets/js/select-images.js
+++ assets/js/select-images.js
@@ -1,17 +1,13 @@
 $(document).ready(function () {
+
   var APIkey = "563492ad6f91700001000001b70fb799bff84ca4b8a69bfeb2fb60e3";
 
-  import { createClient } from "pexels";
+  $.ajax({
+    url: "https://api.pexels.com/v1/search?query=dogs",
+    method: "GET",
+    headers: { Authorization: APIkey },
+  }).then(function (response) {
+    console.log(response);
+  });
 
-  const client = createClient(APIkey);
-  const query = "Nature";
-
-  client.photos
-    .search({
-      query,
-      per_page: 1,
-    })
-    .then(function (photos) {
-      console.log(photos);
-    });
 });
