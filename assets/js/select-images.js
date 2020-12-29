ded33076b9bbd69649ca0a7012103cb5e6695825
Jeana Rose <jr.mathis608@gmail.com>
2020-12-29 18:23:08 -0500

    Added newImage function for event listener to generate a new image when the times button is clicked

--- assets/js/select-images.js
+++ assets/js/select-images.js
@@ -4,12 +4,10 @@ $(document).ready(function () {
   var query = "work";
 
   // Function to keep image when the check mark is clicked
-  function keepImage(event){
+  function keepImage(event) {
     event.preventDefault();
-    $(".check-btn").attr("style","background-color:green");
+    $(".check-btn").attr("style", "background-color:#6dda6dbd");
   }
-
-  
 
   $.ajax({
     url: "https://api.pexels.com/v1/search?query=" + query,
@@ -17,41 +15,42 @@ $(document).ready(function () {
     headers: { Authorization: APIkey },
   }).then(function (response) {
     console.log(response);
-    console.log(response.photos[0].src.original);
-    var randomImage = Math.floor(Math.random() * response.photos.length)
-    // Add header to the page
-    var workDiv = $("#work-image");
-    var workHeader = $("<h2>");
-    workHeader.text("Work");
-    workDiv.append(workHeader);
-    // Create image and add it to the page
-    imageSource = response.photos[randomImage].src.original;
-    var workImage = $("<img>");
-    workImage.attr("src", imageSource);
-    workImage.addClass("image-choice");
-    workDiv.append(workImage);
-    // Create check button
-    var checkBtn = $("<button>");
-    checkBtn.addClass("btn check-btn");
-    var checkIcon = $("<i>");
-    checkIcon.addClass("fas fa-check-square fa-lg");
-    checkBtn.append(checkIcon);
-    // Create times button
-    var timesBtn = $("<button>");
-    timesBtn.addClass("btn times-btn");
-    var timesIcon = $("<i>");
-    timesIcon.addClass("fas fa-times-circle fa-lg");
-    timesBtn.append(timesIcon)
-    // Append buttons to the work div
-    workDiv.append(checkBtn);
-    workDiv.append(timesBtn);
-    // If the check button is clicked, the image stays. If the times button is clicked, a new image is generated
-    function newImage(event){
-      event.preventDefault();
-      // Generate new image using math.random
-      
+    function newImage(event) {
+      var workDiv = $("#work-image");
+      workDiv.empty();
+      var randomImage = Math.floor(Math.random() * response.photos.length);
+      // Add header to the page
+      var workHeader = $("<h2>");
+      workHeader.text("Work");
+      workDiv.append(workHeader);
+      // Create image and add it to the page
+      imageSource = response.photos[randomImage].src.original;
+      var workImage = $("<img>");
+      workImage.attr("src", imageSource);
+      workImage.addClass("image-choice");
+      workDiv.append(workImage);
+      // Create check button
+      var checkBtn = $("<button>");
+      checkBtn.addClass("btn check-btn");
+      var checkIcon = $("<i>");
+      checkIcon.addClass("fas fa-check-square fa-lg");
+      checkBtn.append(checkIcon);
+      // Create times button
+      var timesBtn = $("<button>");
+      timesBtn.addClass("btn times-btn");
+      var timesIcon = $("<i>");
+      timesIcon.addClass("fas fa-times-circle fa-lg");
+      timesBtn.append(timesIcon);
+      // Append buttons to the work div
+      workDiv.append(checkBtn);
+      workDiv.append(timesBtn);
+      console.log("You ran the newImage function!")
     }
-    $(checkBtn).on("click", keepImage);
-    $(timesBtn).on("click", newImage);
+    newImage();
+    // If the check button is clicked, the image stays. If the times button is clicked, a new image is generated
+    $(".check-btn").on("click", keepImage);
+    $(".times-btn").on("click", newImage);
   });
 });
+
+// If check  button color = #6dda6dbd, add to vision board
