57b93aa5d55ab47f3a28a70f26b88a0c7bc87a22
Jeana Rose <jr.mathis608@gmail.com>
2021-01-01 16:33:38 -0500

    Added data attribute for local storage

--- assets/js/issue31.js
+++ assets/js/issue31.js
@@ -6,15 +6,19 @@ $(document).ready(function () {
   // Function to select clicked answer
   function selectAnswer(event) {
     var answerText = $(this)[0].outerText;
+    var question = $("h1").data("question");
     if ($(this).hasClass("active")) {
       $(this).removeClass("active");
       selectedAnswers.splice(answerText);
     } else if (!$(this).hasClass("active")) {
       $(this).addClass("active");
       selectedAnswers.push(answerText);
+      localStorage.setItem("question", selectedAnswers);
       console.log(selectedAnswers);
     }
   }
+
+//   selectedAnswers.data("hours", hoursArray[i]);
 
   // Event listeners
   $(".btn").on("click", selectAnswer);
