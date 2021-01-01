bab65970456beb8506bd09de27d15484f0d06817
Jeana Rose <jr.mathis608@gmail.com>
2021-01-01 14:57:26 -0500

    Added function and conditional to add and remove the active class when each option button is cicked

--- assets/js/issue31.js
+++ assets/js/issue31.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
   function storeAnswer() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+    } else if (!$(this).hasClass("active")) {
+      $(this).addClass("active");
+    }
+
+    console.log($(this));
+    console.log($(this)[0].outerText);
     console.log("You clicked an answer button!");
   }
   $(".btn").on("click", storeAnswer);
