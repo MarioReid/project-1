8fa88e572d30d9f9dec14ce2901e28a86c73fb43
Jeana Rose <jr.mathis608@gmail.com>
2021-01-03 18:20:04 -0500

    Added quote using Best Quotes API

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,24 @@
-// $(document).ready(function () {
-//     var quote = $("#quote")
-// });
+$(document).ready(function () {
+  var quoteDiv = $("#quote");
+  var settings = {
+    async: true,
+    crossDomain: true,
+    url: "https://qvoca-bestquotes-v1.p.rapidapi.com/quote?genre=inspirational",
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "24e948b04dmshd01e0a3e648ff18p1648ddjsn98ffea1eea74",
+      "x-rapidapi-host": "qvoca-bestquotes-v1.p.rapidapi.com",
+    },
+  };
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    console.log(response.message);
+    var quote = $("<h5>");
+    quote.text('"' + response.message + '"');
+    quoteDiv.append(quote);
+    var author = $("<h5>");
+    author.text(" - " + response.author);
+    quoteDiv.append(author);
+  });
+});
