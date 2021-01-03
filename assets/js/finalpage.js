8a5643d5a2f364834fbf51e77d9bd1febb51e649
Jeana Rose <jr.mathis608@gmail.com>
2021-01-03 17:08:45 -0500

    Added format to queryURl

--- assets/js/finalpage.js
+++ assets/js/finalpage.js
@@ -3,11 +3,14 @@ $(document).ready(function () {
   var APIkey = "eaf25249dc224e30241c400939108e64";
   var documentUrl = "https://marioreid.github.io/project-1/finalpage.html";
   var queryUrl =
-  "https://api.screenshotlayer.com/api/capture" + 
-  "?access_key=" + APIkey + 
-  "&url=" + documentUrl +
-  "&viewport=1440x900" + 
-  "&fullpage=1";
+    "https://api.screenshotlayer.com/api/capture" +
+    "?access_key=" +
+    APIkey +
+    "&url=" +
+    documentUrl +
+    "&viewport=1440x900" +
+    "&fullpage=1" +
+    "&format=JPG";
   var visionBoard = JSON.parse(localStorage.getItem("images")) || [];
 
   // Append images from local storage
@@ -21,7 +24,6 @@ $(document).ready(function () {
 
   // AJAX call to download vision board as a PDF
   $.ajax({
-    html: "base64 encoded string",
     url: queryUrl,
     method: "POST",
   }).then(function (response) {
