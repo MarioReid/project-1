3a08ac0201ba5b4eeaaad750e98b7ba9a65f50f2
Miranda McDonald <mirandagrace.dev@gmail.com>
2021-01-04 11:28:48 -0500

    tidied up finalpage js and html

--- assets/js/finalpage.js
+++ assets/js/finalpage.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
 	// Variables
-	var htmlContents;
-	var APIkey = "12d4c39638bec326a8fe210ca42c345e";
-	// var documentUrl = "https://marioreid.github.io/project-1/finalpage.html";
-	var queryUrl =
-		"https://cors-anywhere.herokuapp.com/https://api.pdflayer.com/api/convert" +
-		"?access_key=" +
-		APIkey +
-		"&document_html=" +
-		htmlContents +
-		"&delay=2000" +
-		"&force=1";
-	//https://api.screenshotlayer.com/api/capture?access_key=eaf25249dc224e30241c400939108e64&url=https://marioreid.github.io/project-1/finalpage.html&viewport=1440x900&fullpage=1&delay=2&force=1
+	// var htmlContents;
+	// var APIkey = "12d4c39638bec326a8fe210ca42c345e";
+	// // var documentUrl = "https://marioreid.github.io/project-1/finalpage.html";
+	// var queryUrl =
+	// 	"https://cors-anywhere.herokuapp.com/https://api.pdflayer.com/api/convert" +
+	// 	"?access_key=" +
+	// 	APIkey +
+	// 	"&document_html=" +
+	// 	htmlContents +
+	// 	"&delay=2000" +
+	// 	"&force=1";
+	// //https://api.screenshotlayer.com/api/capture?access_key=eaf25249dc224e30241c400939108e64&url=https://marioreid.github.io/project-1/finalpage.html&viewport=1440x900&fullpage=1&delay=2&force=1
 	var visionBoard = JSON.parse(localStorage.getItem("images")) || [];
 
 	// Append images from local storage
@@ -21,19 +21,5 @@ $(document).ready(function () {
 		var images = $("<img>");
 		images.attr("src", visionBoard[i]);
 		visionBoardDiv.append(images);
-	}
-
-	$(".pdf-btn").on("click", openPdf);
-	// window.open(queryUrl);
-
-	// AJAX call to download vision board as a PDF
-	function openPdf() {
-		htmlContents = document.querySelector("html").innerHTML;
-		$.ajax({
-			url: queryUrl,
-			method: "POST",
-		}).then(function (response) {
-			console.log(response);
-		});
 	}
 });
